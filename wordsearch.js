1e083c7ad09886b8af0643217d9c61464485f7c8
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-11-04 18:20:12 -0500

    Create wordsearch.js

---
+++ wordsearch.js
@@ -0,0 +1,39 @@
+const transpose = function (letters) {
+
+    let newArr = [];
+    for (let k = 0; k < letters[0].length; k++) {
+      newArr.push([]);
+    }
+    for (let i = 0; i < letters.length; i++) {
+      for (let j = 0; j < letters[i].length; j++) {
+        newArr[j].push(letters[i][j]);
+      }
+    }
+    return newArr;
+  };
+  
+  
+  const wordSearch = (letters, word) => {
+    if (letters.length === 0) {
+      return false;
+    }
+  
+    const horizontalJoin = letters.map(ls => ls.join('')); 
+    for (let l of horizontalJoin) { 
+      if (l.includes(word)) {
+        return true;
+      }
+    }
+    const verticalJoin = transpose(letters).map(ls => ls.join('')); 
+    for (let l of verticalJoin) { 
+      if (l.includes(word)) {
+        return true;
+      }
+    }
+    return false;
+  };
+  
+  
+  
+  
+  module.exports = wordSearch;
